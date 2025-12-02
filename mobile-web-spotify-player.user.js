70bfe95a55da2e56a410a3d48a7b990def563382
Felipe Siqueira <felipe.sqr@gmail.com>
2025-12-02 12:48:34 +0100

    Refactor mobile Spotify player script for UI adjustments

--- mobile-web-spotify-player.user.js
+++ mobile-web-spotify-player.user.js
@@ -4,7 +4,7 @@
 // @version      1.2
 // @description  Mobile Spotify web player
 // @author       You
-// @match        https://open.spotify.com/*
+// @match        *://open.spotify.com/*
 // @grant        none
 // ==/UserScript==
 
@@ -16,6 +16,18 @@
       // fix overall mobile resposiveness
       document.body.style.setProperty('min-width', '0', 'important');
 
+      //Hide header spotify icon
+      document.querySelector('button[aria-label="Home"]').style.setProperty('display', 'none', 'important');
+      document.querySelector('button[aria-label="Go back"]').parentElement.style.setProperty('display', 'none', 'important');
+
+      //Hide header friends activity button
+      document.querySelector('button[aria-label="What\'s New"]').style.setProperty('display', 'none', 'important');
+      const fab = document.querySelector('button[aria-label="Friend Activity"]');
+      if (fab) { fab.style.setProperty('display', 'none', 'important'); }
+
+
+      //Hide footerbar left controls
+      document.querySelector('button[aria-label="Queue"]').parentElement.parentElement.parentElement.style.setProperty('display', 'none', 'important');
 
       //Hide left sidebar
       document.getElementById('Desktop_LeftSidebar_Id').style.setProperty('display', 'none', 'important');
@@ -23,46 +35,30 @@
       //Hide right sidebar
       document.getElementById('Desktop_PanelContainer_Id').parentElement.style.setProperty('display', 'none', 'important');
 
-      // fix footerbar mobile resposiveness
-      document.querySelector('button[aria-label="Now playing view"]').parentElement.parentElement.parentElement.parentElement.parentElement.style.setProperty('justify-content', 'normal', 'important');
-      document.querySelector('button[aria-label="Now playing view"]').parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.style.setProperty('min-width', '0', 'important');
-
-      //center and widen footerbar player controls
-      document.querySelector('button[aria-label="Previous"]').parentElement.parentElement.parentElement.parentElement.style.setProperty('width', '100%');
-
-      //Hide footerbar left controls
-      document.querySelector('button[aria-label="Lyrics"]').parentElement.parentElement.style.setProperty('display', 'none', 'important');
+      //Hide footer
+      document.querySelectorAll('.main-view-container__mh-footer-container').forEach(el => {el.style.setProperty('display', 'none', 'important');});
 
       //Hide footerbar right controls
       document.querySelector('button[aria-label="Add to Liked Songs"]').parentElement.parentElement.parentElement.style.setProperty('display', 'none', 'important');
       document.querySelector('button[aria-label="Collapse"]').parentElement.parentElement.parentElement.style.setProperty('display', 'none', 'important');
 
+      //center and widen footerbar player controls
+      document.querySelector('button[aria-label="Previous"]').parentElement.parentElement.parentElement.parentElement.style.setProperty('width', '100%');
+
+
+      // fix footerbar mobile resposiveness
+      document.querySelector('button[aria-label="Now playing view"]').parentElement.parentElement.parentElement.parentElement.parentElement.style.setProperty('justify-content', 'normal', 'important');
+      document.querySelector('button[aria-label="Now playing view"]').parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.style.setProperty('min-width', '0', 'important');
+
+
       //Hide media type pills
       document.querySelector('button[aria-label="Audiobooks"]').parentElement.parentElement.parentElement.parentElement.parentElement.style.setProperty('display', 'none', 'important');
 
-
-
-      //Hide header friends activity button
-      document.querySelector('button[aria-label="Friend Activity"]').style.setProperty('display', 'none', 'important');
-      document.querySelector('button[aria-label="What\'s New"]').style.setProperty('display', 'none', 'important');
-
-
-      //Hide footer
-      document.querySelectorAll('.main-view-container__mh-footer-container').forEach(el => {el.style.setProperty('display', 'none', 'important');});
-
-      //Hide header spotify icon
-      document.querySelector('button[aria-label="Home"]').style.setProperty('display', 'none', 'important');
-      document.querySelector('button[aria-label="Go back"]').style.setProperty('display', 'none', 'important');
-      document.querySelector('button[aria-label="Go forward"]').style.setProperty('display', 'none', 'important');
-
-
-
-
     }
 
-
     const observer = new MutationObserver(() => {
-        hideElements();
+      // setTimeout(function() { hideElements(); }, 2000);
+      hideElements()
     });
 
     observer.observe(document.body, {
